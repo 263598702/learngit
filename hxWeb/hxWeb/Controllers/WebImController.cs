0c8ac457fe6e42b38de88a63b9ee2faed4b27bb2
gg <263598702@qq.com>
2017-02-22 17:42:26 +0800

    ddd

---
+++ hxWeb/hxWeb/Controllers/WebImController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hxWeb.Controllers
+{
+    public class WebImController : Controller
+    {
+        // GET: WebIm
+        public ActionResult Index(string token)
+        {
+            ViewBag.token = token;
+            return View();
+        }
+    }
+}
---
+++ hxWeb/hxWeb/Scripts/demo.js
@@ -0,0 +1,42 @@
+﻿var Demo= {
+    createConn:function() {
+        var conn = new WebIM.connection({
+            https: WebIM.config.https,
+            url: WebIM.config.xmppURL,
+            isAutoLogin: WebIM.config.isAutoLogin,
+            isMultiLoginSessions: WebIM.config.isMultiLoginSessions
+        });
+        return conn;
+    },
+    login: function (uname, pwd) {
+        var t;
+        var conn = Demo.createConn();
+        var options = {
+            apiUrl: WebIM.config.apiURL,
+            user: uname,
+            pwd: pwd,
+            appKey: WebIM.config.appkey,
+            success: function (token) {
+                token = token.access_token; 
+                t = token;
+                WebIM.utils.setCookie('webim_' + uname, token, 1);
+
+                conn.open(options);
+                return t;
+            },
+            error:function(e) {
+                alert(e)
+            }
+        };
+    },
+    openConn: function (token) { 
+        var options = {
+            apiUrl: WebIM.config.apiURL,
+            accessToken: token,
+            appKey: WebIM.config.appkey
+        };
+        var conn = Demo.createConn();
+        conn.open(options);
+        return conn;
+    }
+}
